195a58c4c67e4829c31d6b9202f795a77237c28f
21BCA64 <96609082+21BCA64@users.noreply.github.com>
2022-03-24 11:18:53 +0530

    Add files via upload

---
+++ solution/characterlength.py
@@ -0,0 +1,14 @@
+#was to enter 5 string in a list and check and print string whose length has even num of character.
+def createlist():
+    l=[]
+    for i in range(5):
+        s=input("Enter the string:")
+        l.append(s)
+    return(l)
+a=createlist()
+print(a)
+
+
+
+
+
